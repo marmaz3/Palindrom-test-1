7d9e2262642969bb41f76bcc508ca66caff66a34
marmaz3 <mazur.marekk88@gmail.com>
2023-03-27 16:57:13 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string slowo;
+            bool isPalindrom = true;
+        slowo = Console.ReadLine().Replace(" ", string.Empty).ToLower();
+        for (int i = 0; i < slowo.Length / 2; i++)
+        {
+            if (slowo[i] != slowo[slowo.Length - 1 - i])
+            {
+                isPalindrom = false;
+                break;
+            }
+        }
+        if (isPalindrom)
+        {
+            Console.WriteLine("Tak");
+        }
+        else
+        {
+            Console.WriteLine("Nie");
+        }
+
+    }
+} 
+    
